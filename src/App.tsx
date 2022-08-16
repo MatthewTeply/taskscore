13aa247a284dcad886fe880d53cc09abb19aa9a8
Matyáš Teplý <matyas.teply@lmc.eu>
2022-08-16 19:15:32 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (): React.ReactElement {
+	function saySomething (
+		thingOne: string,
+		thingTwo: string
+	): string {
+		return thingOne + ' ' + thingTwo;
+	}
+
+	return (
+		<div className="App">
+			{saySomething('Hello', 'there')}
+		</div>
+	);
 }
 
 export default App;
